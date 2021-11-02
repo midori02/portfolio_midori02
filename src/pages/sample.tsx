29f69013e48e54216dd48a393ed2f4db522cdae8
yabu midori <midori00yabu@gmail.com>
2021-11-02 14:27:12 +0900

    reset layout

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,25 +1,7 @@
-import { Button } from 'components/atoms/Buttons'
-import { InputArea } from 'components/atoms/Texts'
-import { Footer, Header } from 'components/organisms'
-import { ContactForm } from 'components/molecules/ContactForm'
 import { NextPage } from 'next'
-import { AboutContainer, WorksContainer } from 'components/containers'
-import WorksLinkComponent from 'components/molecules/Link/ WorksLinkComponent '
-import AutoSlideAnimation from 'components/molecules/Works/AutoSlideAnimation'
 
 const Sample: NextPage = () => {
-  return (
-    <>
-      <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <WorksContainer />
-    </>
-  )
+  return <></>
 }
 
 export default Sample
