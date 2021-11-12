13c4419522527e01d046d7bc1632305d7675efb8
yabu midori <midori00yabu@gmail.com>
2021-11-13 05:56:51 +0900

    fixed sp layout

--- src/pages/[works].tsx
+++ src/pages/[works].tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 
+import { Spacer } from 'components/atoms/Spacer'
 import { WorkPageContainer } from 'components/containers'
 
 const Works: FC = () => {
   return (
     <>
       <WorkPageContainer />
+      <Spacer size={'lg_h'} />
     </>
   )
 }
