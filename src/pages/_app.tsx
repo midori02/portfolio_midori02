230af1c4e71a275f90f7c04ff1bbb82f53136ff4
yabu midori <midori00yabu@gmail.com>
2022-01-11 11:54:21 +0900

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { NextSeo } from 'next-seo'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
@@ -25,6 +26,28 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <QueryClientProvider client={queryClient} contextSharing>
+      <NextSeo
+        title="midori02--portfolio"
+        description="This Page is midori02's portfolio..."
+        openGraph={{
+          type: 'website',
+          url: 'https://www.midori02.com/',
+          title: 'midori02',
+          description: "This Page is midori02's portfolio...",
+          images: [
+            {
+              url: 'https://i.imgur.com/eIFttvZ.png',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+          ],
+          profile: {
+            firstName: 'midori02',
+            username: 'midori02',
+          },
+        }}
+      />
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
