6317f8712f06347e8b1ea9543354c4a8c00a1a85
yabu midori <midori00yabu@gmail.com>
2022-01-21 09:33:50 +0900

    add Instagram Recommendation

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react'
 
+import { IconArea } from 'components/atoms/Images'
 import { SNSLinkComponent } from 'components/molecules/Link'
 import styles from 'styles/components/organisms/footer.module.scss'
+
 const Footer: FC = () => {
   return (
     <div className={styles.footer}>
+      <div className={styles.footer__text}>
+        <p>My Everyday Practice Here. Check it!</p>
+        <img src={'/arrow.png'} alt="icon"></img>
+      </div>
       <SNSLinkComponent />
       <div className={styles.footer__copywrite}>© midori02 All rights reserved.</div>
     </div>
